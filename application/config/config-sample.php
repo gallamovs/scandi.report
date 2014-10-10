e83dfe3f988ba8ad4bf9e9b785de91251e61fe5b
Oskars Tuns <oskars@scandiweb.com>
2014-10-10 19:43:02 +0300

    sample-config

--- application/config/config-sample.php
+++ application/config/config-sample.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://dev.scandi.report';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = 'dfgsdfbrtyu';
+$config['encryption_key'] = '';
 
 /*
 |--------------------------------------------------------------------------
